c26f2be7dad17e17dd49593fc1a04e767d3b39b6
nenktisya <nenktisya22@gmaill.com>
2024-12-27 14:38:10 +0700

    edit footer

--- resources/views/admin/banner.blade.php
+++ resources/views/admin/banner.blade.php
@@ -2,43 +2,43 @@
 
     <!-- Table Start -->
     <div class="container-fluid pt-4 px-4">
-            <div class="row g-4">
-                <div class="col-sm-12 col-xl-12">
-                    <div class="bg-light rounded h-100 p-4">
-                            <h6 class="mb-4">Tombol Tambah Data Banner</h6>
-                            <div class="m-n2">
-                                <a href="{{route('addbanner')}}"><button type="button" class="btn btn-primary m-2">Tambah Data</button></a>
-                            </div>
-                        </div>
-                    </div>
-                    <h6 class="mb-4">Responsive Table</h6>
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Judul</th>
-                                    <th scope="col">Desktipsi</th>
-                                    <th scope="col">Gambar</th>
-                                    <th scope="col">Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>John</td>
-                                    <td>Doe</td>
-                                    <td>jhon@email.com</td>
-                                    <td>
-                                        <a href="#"><button type="button" class="btn btn-warning m-2">edit</button></a>
-                                        <a href="#"><button type="button" class="btn btn-danger m-2">Hapus</button></a>
-                                    </td>
-                                </tr>
-
-                            </tbody>
-                        </table>
+        <div class="row g-4">
+            <div class="col-sm-12 col-xl-12">
+                <div class="bg-light rounded h-100 p-4">
+                    <h6 class="mb-4">Tombol Tambah Data Banner</h6>
+                    <div class="m-n2">
+                        <a href="{{route('addbanner')}}">
+                            <button type="button" class="btn btn-primary m-2">Tambah Data</button>
+                        </a>
                     </div>
                 </div>
+            </div>
+            <h6 class="mb-4">Responsive Table</h6>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Judul</th>
+                            <th scope="col">Desktipsi</th>
+                            <th scope="col">Gambar</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>John</td>
+                            <td>Doe</td>
+                            <td>jhon@email.com</td>
+                            <td>
+                                <a href="#"><button type="button" class="btn btn-warning m-2">edit</button></a>
+                                <a href="#"><button type="button" class="btn btn-danger m-2">Hapus</button></a>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
